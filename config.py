991ffa40ea783d5826a818745959021e0166d5ec
Yoav Ram <yoav@yoavram.com>
2025-06-28 17:57:52 +0300

    fix refactoring bugs

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+import os
+
+
 DATA_FOLDER   = Path("data/")
 CHECKPOINTS_FOLDER = Path("checkpoints/")
 os.makedirs(CHECKPOINTS_FOLDER, exist_ok=True)
 RESULTS_FOLDER =  Path("results/")
 os.makedirs(RESULTS_FOLDER, exist_ok=True)
+SEGMENTED_FOLDER = Path("segmented")
+os.makedirs(SEGMENTED_FOLDER, exist_ok=True)
